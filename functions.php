7aeb6054f2a1c246b7c1ff5c10e704f2d359d915
wendigotech <wendigotech@gmail.com>
2017-10-27 05:06:28 +0200

    enque woo.css in functions.php

--- functions.php
+++ functions.php
@@ -186,14 +186,13 @@ if ( ! function_exists( 'pg_starter_enqueue_scripts' ) ) :
 
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', null, '1.2', 'all' );
 
+    wp_enqueue_style( 'woo', get_template_directory_uri() . '/woo.css', null, null, 'all' );
+
     wp_deregister_style( 'style-1' );
     wp_enqueue_style( 'style-1', 'https://fonts.googleapis.com/css?family=Dosis:500|Indie+Flower', false, null, 'all');
 
     wp_deregister_style( 'freelancer' );
     wp_enqueue_style( 'freelancer', get_template_directory_uri() . '/components/freelancer/css/freelancer.css', false, null, 'all');
-
-    wp_deregister_style( 'woo' );
-    wp_enqueue_style( 'woo', get_template_directory_uri() . '/woo.css', false, null, 'all');
 
     /* Pinegrow generated Enqueue Styles End */
 
